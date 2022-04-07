2a21c22f3cd37103d15ed1483142164930189c35
XiaoDHuang <huangdongdong3@qq.com>
2022-04-07 14:52:00 +0800

    feat: 通过webppack resolve.symlinks解决链接文件位置问题

--- src/index.js
+++ src/index.js
@@ -4,40 +4,45 @@ const fs = require('fs');
 
 class PnpmResolverPlugin {
   apply(compiler) {
+    const loaderContext = `${compiler.context}/node_modules/.pnpm`
+    const isPnpmModule = fs.existsSync(loaderContext);
+    if (!isPnpmModule) return;
+
+
+    compiler.options.resolve.modules.push(loaderContext)
+    compiler.options.resolve.symlinks = true;
+
     compiler.hooks.compilation.tap('PnpmResolverPlugin', (compilation, {normalModuleFactory}) => {
-      const loaderContext = `${compiler.context}/node_modules/.pnpm`
-      const isPnpmModule = fs.existsSync(loaderContext);
-
-      if (!isPnpmModule) return;
-
-      // 这里主要处理 css-loader babel-loader 内置到公司内部脚手架问题
+    
+      // 这里主要处理 css-loader babel-loader 内置到公司内部脚手架的问题
       normalModuleFactory.context = loaderContext;
-
+      
+      // compiler.options.resolve.symlinks = true; 解决实际路径问题
       // 这里主要处理模块真实路径问题
-      normalModuleFactory.hooks.afterResolve.tap('PnpmResolverPlugin', (result) => {
-        try {
-          const resource = fs.realpathSync(result.resource);
-          const context = getContext(resource);
+      // normalModuleFactory.hooks.afterResolve.tap('PnpmResolverPlugin', (result) => {
+      //   try {
+      //     const resource = fs.realpathSync(result.resource);
+      //     const context = getContext(resource);
 
-          if (result.userRequest === result.resource) {
-            result.userRequest = resource;
-          }
+      //     if (result.userRequest === result.resource) {
+      //       result.userRequest = resource;
+      //     }
 
-          if (result.request === result.resource) {
-            result.request = resource;
-          }
+      //     if (result.request === result.resource) {
+      //       result.request = resource;
+      //     }
 
-          result.resource = resource;
-          result.context = context;
+      //     result.resource = resource;
+      //     result.context = context;
 
-          result.resourceResolveData.descriptionFilePath = fs.realpathSync(result.resourceResolveData.descriptionFilePath);
-          result.resourceResolveData.descriptionFileRoot = fs.realpathSync(result.resourceResolveData.descriptionFileRoot);
-          result.resourceResolveData.path = fs.realpathSync(result.resourceResolveData.path);
+      //     result.resourceResolveData.descriptionFilePath = fs.realpathSync(result.resourceResolveData.descriptionFilePath);
+      //     result.resourceResolveData.descriptionFileRoot = fs.realpathSync(result.resourceResolveData.descriptionFileRoot);
+      //     result.resourceResolveData.path = fs.realpathSync(result.resourceResolveData.path);
 
-        } catch(err) {
-          // debugger;
-        }
-      })
+      //   } catch(err) {
+      //     // debugger;
+      //   }
+      // })
 
     });
   }
